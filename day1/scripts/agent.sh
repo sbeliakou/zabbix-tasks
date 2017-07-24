3efdbbe04f408beb4be6530cb7d41c20d9eb792e
SVLay <ssvlay@gmail.com>
2017-07-24 22:41:38 +0300

    Update agent.sh

--- day1/scripts/agent.sh
+++ day1/scripts/agent.sh
@@ -4,6 +4,6 @@ echo Installing Zabbix Agent
 yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
 yum -y install zabbix-sender
 yum -y install zabbix-agent
-sed -i -e 's:127.0.0.1:192.168.56.51:g' /etc/zabbix/zabbix_agentd.conf
+sed -i -e 's:127.0.0.1:192.168.56.11:g' /etc/zabbix/zabbix_agentd.conf
 
 systemctl start zabbix-agent
