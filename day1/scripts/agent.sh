f2346f29d82e5f736fd288c337fd34308e63bc93
marijademenkova <mar.demenkova@gmail.com>
2017-07-25 02:38:43 +0300

    Update agent.sh

--- day1/scripts/agent.sh
+++ day1/scripts/agent.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 echo Installing Zabbix Agent
+yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+yum -y install zabbix-agent
+systemctl start zabbix-agent
+
