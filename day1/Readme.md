a3fb66a018bd509f30ffb043bb5e586acb28c183
SuperBazis <bazi_s@mail.ru>
2017-07-24 23:08:23 +0300

    Download

--- day1/Readme.md
+++ day1/Readme.md
@@ -1,2 +1,6 @@
 # Report Here
+##  Using Zabbix UI:
+* Create User group “Project Owners” 
+<img src="Screens/Screenshot from 2017-07-24 18-22-25.png">
+<img src="Screens/Screenshot from 2017-07-24 18-29-39.png">
 
--- day1/scripts/agent.sh
+++ day1/scripts/agent.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 
 echo Installing Zabbix Agent
+
+yum install -y vim;
+yum install -y net-tools;
+yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm;
+yum -y install zabbix-agent;
+
+
+sed -i 's|# DebugLevel=3|DebugLevel=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Server=127.0.0.1|Server=192.168.56.10|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenPort=10050|ListenPort=10050|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenIP=0.0.0.0|ListenIP=0.0.0.0|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# StartAgents=3|StartAgents=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|ServerActive=127.0.0.1|ServerActive=192.168.56.10:10051|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Hostname=Zabbix server|Hostname=Zabbix client|' /etc/zabbix/zabbix_agentd.conf;
+
+systemctl start zabbix-agent;
+yum -y install zabbix-sender;
+yum -y install zabbix-get;
+
