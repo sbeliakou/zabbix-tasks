f8dcda970925884ce66a22103d688dc0cd5810ee
Zakhar Virinsky <virinsky@gmail.com>
2017-07-24 21:31:35 +0300

    installing of zabbix-sender and zabbix-get added

--- day1/scripts/agent.sh
+++ day1/scripts/agent.sh
@@ -3,6 +3,8 @@
 #agent
 yum install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm -y
 yum install zabbix-agent -y
+yum install zabbix-sender -y
+yum install zabbix-get -y
 
 if [ "$HOSTNAME" = zabbix-client ]; then
   sed -i 's/Server=127.0.0.1/Server=192.168.56.105/g' /etc/zabbix/zabbix_agentd.conf
