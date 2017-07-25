0b96632aa9a57550944909d151280d6f29f0c057
SVLay <ssvlay@gmail.com>
2017-07-25 23:08:23 +0300

    Create server.sh

---
+++ day2/server.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+echo Installing Zabbix Server
+yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+yum -y install mariadb mariadb-server
+/usr/bin/mysql_install_db --user=mysql
+systemctl start mariadb
+yum -y install zabbix-server-mysql zabbix-web-mysql
+sed -i '/# DBPassword=/a DBPassword=zabbixDB' /etc/zabbix/zabbix_server.conf
+mysql -uroot -e "create database zabbix character set utf8 collate utf8_bin;"
+mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDB';"
+zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -uzabbix -pzabbixDB zabbix
+
+
+yum -y install zabbix-java-gateway
+systemctl start zabbix-java-gateway
+systemctl enable zabbix-java-gateway
+
+sed -i '/# JavaGateway=/aJavaGateway=192.168.56.10' /etc/zabbix/zabbix_server.conf
+sed -i '/# JavaGatewayPort=10052/aJavaGatewayPort=10052' /etc/zabbix/zabbix_server.conf
+sed -i '/# StartJavaPollers=0/aStartJavaPollers=5' /etc/zabbix/zabbix_server.conf
+
+
+
+systemctl start zabbix-server 
+sed -i '6a RedirectMatch ^/$ http://192.168.56.10/zabbix/' /etc/httpd/conf.d/zabbix.conf
+sed -i -e 's:# php_value date.timezone Europe/Riga:php_value date.timezone Europe/Minsk:g' /etc/httpd/conf.d/zabbix.conf
+systemctl start httpd 
+		cat > /etc/zabbix/web/zabbix.conf.php << 'EOL'
+<?php
+// Zabbix GUI configuration file.
+global $DB;
+
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'zabbix';
+$DB['PASSWORD'] = 'zabbixDB';
+
+// Schema name. Used for IBM DB2 and PostgreSQL.
+$DB['SCHEMA'] = '';
+
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'zabbix-server';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+EOL
