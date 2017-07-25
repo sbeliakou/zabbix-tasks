31cc381773c0d86c2f83ccce8f01952bcf8308e6
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-25 17:24:02 +0300

    readme edit

---
+++ day2/scripts/python_api.py
@@ -0,0 +1,26 @@
+import os, requests, json, sys
+from requests.auth import HTTPBasicAuth
+
+zabbix_server = "..."
+zabbix_api_admin_name = "..."
+zabbix_api_admin_password = "..."
+
+def post(request):
+    headers = {'content-type': 'application/json'}
+    return requests.post(
+        "http://" + zabbix_server + "/api_jsonrpc.php",
+         data=json.dumps(request),
+         headers=headers,
+         auth=HTTPBasicAuth(zabbix_api_admin_name, zabbix_api_admin_password)
+    )
+
+auth_token = post({
+    "jsonrpc": "2.0",
+    "method": "user.login",
+    "params": {
+         "user": zabbix_api_admin_name,
+         "password": zabbix_api_admin_password
+     },
+    "auth": None,
+    "id": 0}
+).json()["result"]
