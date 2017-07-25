28c8ecf25ab6c9f19568b98f48738a2623d0b405
Vadzim Tarasiuk <vad.tarasyuk@gmail.com>
2017-07-25 11:51:25 +0300

    Initial Vagrant scripts

---
+++ day2/other.sh
@@ -0,0 +1,16 @@
+yum install zip unzip net-tools vim tomcat tomcat-webapps -y
+
+###TOMCAT###
+systemctl enable tomcat
+systemctl start tomcat
+
+###Z-AGENT###
+yum install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm -y
+yum install zabbix-agent -y
+sed -i 's/# HostnameItem=system.hostname/HostnameItem=system.hostname/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/ServerActive=127.0.0.1/ServerActive=111.111.11.11/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/Server=127.0.0.1/Server=111.111.11.11/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# ListenPort=10050/ListenPort=10050/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# ListenIP=0.0.0.0/ListenIP=0.0.0.0/' /etc/zabbix/zabbix_agentd.conf
+sed -i 's/# StartAgents=3/StartAgents=3/' /etc/zabbix/zabbix_agentd.conf
+systemctl start zabbix-agent
