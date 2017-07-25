e895bd1658f3ac296ced3321c446636310548274
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-25 17:55:47 +0300

    python commit

--- day2/scripts/python_api.py
+++ day2/scripts/python_api.py
@@ -1,9 +1,12 @@
-import os, requests, json, sys
+import os
+import requests
+import json
+import sys
 from requests.auth import HTTPBasicAuth
 
-zabbix_server = "..."
-zabbix_api_admin_name = "..."
-zabbix_api_admin_password = "..."
+zabbix_server = "http://zabbix"
+zabbix_api_admin_name = "Admin"
+zabbix_api_admin_password = "zabbix"
 
 def post(request):
     headers = {'content-type': 'application/json'}
@@ -13,8 +16,6 @@ def post(request):
          headers=headers,
          auth=HTTPBasicAuth(zabbix_api_admin_name, zabbix_api_admin_password)
     )
-
-
 
 auth_token = post({
     "jsonrpc": "2.0",
@@ -26,3 +27,30 @@ auth_token = post({
     "auth": None,
     "id": 0}
 ).json()["result"]
+
+def register_host(hostname, ip):
+        a = post({
+            "jsonrpc": "2.0",
+            "method": "host.create",
+            "params": {
+                "host": hostname,
+                "templates": [{
+                    "templateid": "10001"
+                }],
+                "interfaces": [{
+                    "type": 1,
+                    "main": 1,
+                    "useip": 1,
+                    "ip": ip,
+                    "dns": "",
+                    "port": "10050"
+                }],
+                "groups": [
+                    {"groupid": "1"},
+                    {"groupid": "2"}
+                ]
+            },
+            "auth": auth_token,
+            "id": 1
+        })
+        return a
