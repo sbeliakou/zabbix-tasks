1a906e1c70d864bbe97ccbd19eae0367b232fb76
aion3181 <aion3181@gmail.com>
2017-07-26 18:24:33 +0300

    Update script2.sh

--- day2/script2.sh
+++ day2/script2.sh
@@ -1,6 +1,16 @@
 #Installing zabbix-agent
 yum install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm -y
 yum install zabbix-agent -y
+
+sed -i 's|# DebugLevel=3|DebugLevel=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Server=127.0.0.1|Server=192.168.56.10|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenPort=10050|ListenPort=10050|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenIP=0.0.0.0|ListenIP=0.0.0.0|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# StartAgents=3|StartAgents=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|ServerActive=127.0.0.1|ServerActive=192.168.56.10:10051|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Hostname=Zabbix server|Hostname=zabbix2|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# HostnameItem=system.hostname|HostnameItem=system.hostname|' /etc/zabbix/zabbix_agentd.conf;
+
 systemctl enable zabbix-agent
 systemctl start zabbix-agent
 
@@ -37,6 +47,23 @@ ExecStop=/usr/bin/python /home/vagrant/zabbixhostadd.py down
 Type=forking
 PIDFile=path_to_pidfile
 RemainAfterExit=yes
+
+[Install]
+WantedBy=default.target
+EOL
+
+systemctl daemon-reload
+systemctl enable mypy.service
+systemctl start mypy.service
+fi
+fi
+EOL
+
+systemctl daemon-reload
+systemctl enable mypy.service
+systemctl start mypy.service
+fi
+fi
 
 [Install]
 WantedBy=default.target
