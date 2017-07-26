049fc6bb9af295f322b8faa1393c918d7e72673b
marijademenkova <mar.demenkova@gmail.com>
2017-07-26 21:14:12 +0300

    Update server.sh

--- day2/server.sh
+++ day2/server.sh
@@ -6,13 +6,14 @@ systemctl enable mariadb
 mysql -uroot -e "create database zabbix character set utf8 collate utf8_bin;"
 mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'password';"
 yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
-yum -y install zabbix-server-mysql zabbix-web-mysql
+yum -y install zabbix-server-mysql zabbix-web-mysql zabbix-agent
 zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -uzabbix -ppassword zabbix
 sed -i '/# DBPassword=/a DBPassword=password' /etc/zabbix/zabbix_server.conf
 sed -i '220aJavaGateway=192.168.56.70' /etc/zabbix/zabbix_server.conf
 sed -i '228aJavaGatewayPort=10052' /etc/zabbix/zabbix_server.conf
 sed -i '236aStartJavaPollers=5' /etc/zabbix/zabbix_server.conf
 sed -i 's@# php_value date.timezone Europe/Riga@php_value date.timezone Europe/Minsk@g' /etc/httpd/conf.d/zabbix.conf
+sed -i '6a RedirectMatch ^/$ http://192.168.56.70/zabbix/' /etc/httpd/conf.d/zabbix.conf 
 cat > /etc/zabbix/web/zabbix.conf.php << 'EOL'
 <?php
 // Zabbix GUI configuration file.
@@ -35,6 +36,7 @@ systemctl start zabbix-java-gateway
 systemctl enable zabbix-java-gateway
 systemctl start zabbix-server
 systemctl enable zabbix-server
-sed -i '6a RedirectMatch ^/$ http://192.168.56.70/zabbix/' /etc/httpd/conf.d/zabbix.conf
+systemctl start zabbix-agent
+systemctl enable zabbix-agent 
 systemctl start httpd
 systemctl enable httpd 
