b893b1384368086302b394e6d2e81d2e8f674895
Asemirski <al.semirski@gmail.com>
2017-07-26 16:02:24 +0300

    tomcat fixed

--- day2/scripts/tomcat.sh
+++ day2/scripts/tomcat.sh
@@ -2,9 +2,11 @@
 sed -i 's/enabled=1/enabled=0/g' /etc/yum/pluginconf.d/fastestmirror.conf 
 yum install tomcat -y
 yum install tomcat tomcat-webapps -y
-systemctl start tomcat
-systemctl enable tomcat
+
 cd /usr/share/tomcat/lib
 sudo wget http://central.maven.org/maven2/org/apache/tomcat/tomcat-catalina-jmx-remote/7.0.4/tomcat-catalina-jmx-remote-7.0.4.jar
 sed -i '35 s;^;<Listener className="org.apache.catalina.mbeans.JmxRemoteLifecycleListener" rmiRegistryPortPlatform="8097" rmiServerPortPlatform="8098" />;' /etc/tomcat/server.xml
 sudo echo 'JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=192.168.56.11 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12346 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"' >> /etc/tomcat/tomcat.conf 
+
+systemctl start tomcat
+systemctl enable tomcat
