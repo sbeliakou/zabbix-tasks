1a86f005eaf0acc27e62ada2ac37f01e1b6564ed
marijademenkova <mar.demenkova@gmail.com>
2017-07-26 21:16:41 +0300

    Update agent.sh

--- day2/agent.sh
+++ day2/agent.sh
@@ -7,7 +7,7 @@ sed -i '103aListenPort=10050' /etc/zabbix/zabbix_agentd.conf
 sed -i '111aListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf
 sed -i '136aServerActive=192.168.56.70' /etc/zabbix/zabbix_agentd.conf
 systemctl start zabbix-agent
-systemctl enabled zabbix-agent
+systemctl enable zabbix-agent
 yum -y install tomcat tomcat-webapps -y
 sed -i '35a <Listener className="org.apache.catalina.mbeans.JmxRemoteLifecycleListener" rmiRegistryPortPlatform="8097" rmiServerPortPlatform="8098" />' /usr/share/tomcat/conf/server.xml
 echo 'JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=192.168.56.71 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=8097 -Dcom.sun.management.jmxremote.rmi.port=8098 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"' >> /usr/share/tomcat/conf/tomcat.conf
@@ -15,8 +15,8 @@ cd /usr/share/tomcat/lib/
 wget http://central.maven.org/maven2/org/apache/tomcat/tomcat-catalina-jmx-remote/7.0.69/tomcat-catalina-jmx-remote-7.0.69.jar
 cd /usr/share/tomcat/webapps/
 wget https://tomcat.apache.org/tomcat-6.0-doc/appdev/sample/sample.war
-service tomcat start
-service tomcat enabled
+systemctl start tomcat
+systemctl enable tomcat
 yum -y install python2-pip.noarch
 pip install requests
 wget https://raw.githubusercontent.com/marijademenkova/zabbix-tasks/day2/day2/zabbix.py -P /etc/systemd/system/
