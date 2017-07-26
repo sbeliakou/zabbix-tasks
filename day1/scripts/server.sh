fd72b582eaf2882522ad417fa6868b5e7fc9f427
SVLay <ssvlay@gmail.com>
2017-07-26 13:38:14 +0300

    Update server.sh

--- day1/scripts/server.sh
+++ day1/scripts/server.sh
@@ -5,14 +5,17 @@ yum -y install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.
 yum -y install mariadb mariadb-server
 /usr/bin/mysql_install_db --user=mysql
 systemctl start mariadb
+systemctl enable mariadb
 yum -y install zabbix-server-mysql zabbix-web-mysql
 sed -i '/# DBPassword=/a DBPassword=zabbixDB' /etc/zabbix/zabbix_server.conf
 mysql -uroot -e "create database zabbix character set utf8 collate utf8_bin;"
 mysql -uroot -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixDB';"
 zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -uzabbix -pzabbixDB zabbix
 systemctl start zabbix-server 
+systemctl enable zabbix-server
 sed -i -e 's:# php_value date.timezone Europe/Riga:php_value date.timezone Europe/Minsk:g' /etc/httpd/conf.d/zabbix.conf
 systemctl start httpd 
+systemctl enable httpd 
 		cat > /etc/zabbix/web/zabbix.conf.php << 'EOL'
 <?php
 // Zabbix GUI configuration file.
