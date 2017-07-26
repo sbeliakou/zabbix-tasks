3a1606126dfba96d2b0fddbfaf3be54ec8e60536
aion3181 <aion3181@gmail.com>
2017-07-26 19:09:07 +0300

    Update script.sh

--- day1/scripts/script.sh
+++ day1/scripts/script.sh
@@ -1,6 +1,7 @@
 # Installing and configuring MySQL DB (MariaDB)
 yum install mariadb mariadb-server -y
 /usr/bin/mysql_install_db --user=mysql
+systemctl enable mariadb
 systemctl start mariadb
 mysql -uroot -Bse "create database zabbix character set utf8 collate utf8_bin; 
 grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
@@ -34,6 +35,7 @@ $ZBX_SERVER_NAME = 'Zabbix server';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
 EOL
+systemctl enable zabbix-server
 systemctl start zabbix-server
 
 cat >/etc/httpd/conf.d/zabbix.conf << 'EOI'
@@ -79,13 +81,22 @@ EOI
 systemctl restart httpd
 
 #Installing zabbix-agent
-#yum install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm -y
-#yum install zabbix-web-mysql -y
 
 yum install zabbix-agent -y
+
+sed -i 's|# DebugLevel=3|DebugLevel=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Server=127.0.0.1|Server=192.168.56.10|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenPort=10050|ListenPort=10050|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenIP=0.0.0.0|ListenIP=0.0.0.0|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# StartAgents=3|StartAgents=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|ServerActive=127.0.0.1|ServerActive=192.168.56.10:10051|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Hostname=Zabbix server|Hostname=zabbix|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# HostnameItem=system.hostname|HostnameItem=system.hostname|' /etc/zabbix/zabbix_agentd.conf;
+
+
+systemctl enable zabbix-agent
 systemctl start zabbix-agent
 yum install zabbix-sender -y
 yum install zabbix-get -y
-
 
 
