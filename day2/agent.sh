eb69ac7b95aa0cbc16276533bd9f1b3d59e2aad3
SVLay <ssvlay@gmail.com>
2017-07-26 21:33:28 +0300

    Update agent.sh

--- day2/agent.sh
+++ day2/agent.sh
@@ -13,8 +13,16 @@ sed -i '103aListenPort=10050' /etc/zabbix/zabbix_agentd.conf
 sed -i '112aListenIP=0.0.0.0' /etc/zabbix/zabbix_agentd.conf
 sed -i '122aStartAgents=3' /etc/zabbix/zabbix_agentd.conf
 
+wget https://raw.githubusercontent.com/SVLay/zabbix-tasks/day2/day2/register_host.py -P /etc/systemd/system/
+cat > /etc/systemd/system/register_host.service << EOL
+[Service]
+ExecStart=/bin/python /etc/systemd/system/register_host.py
+EOL
+systemctl enable register_host.service
+systemctl start register_host.service
 
 systemctl start zabbix-agent
+systemctl enable zabbix-agent
 
 yum -y install tomcat tomcat-webapps
 sed -i '$a JAVA_OPTS="-Dcom.sun.management.jmxremote -Djava.rmi.server.hostname=192.168.56.11 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.port=12345 -Dcom.sun.management.jmxremote.rmi.port=12346 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"' /usr/share/tomcat/conf/tomcat.conf
