490b8cac58f3e139daabd63a9aca4ef96ca31872
aion3181 <aion3181@gmail.com>
2017-07-26 18:25:14 +0300

    Update script.sh

--- day2/script.sh
+++ day2/script.sh
@@ -81,18 +81,23 @@ EOI
 systemctl restart httpd
 
 #Installing zabbix-agent
-#yum install http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm -y
-#yum install zabbix-web-mysql -y
 
 yum install zabbix-agent -y
+
+sed -i 's|# DebugLevel=3|DebugLevel=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Server=127.0.0.1|Server=192.168.56.10|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenPort=10050|ListenPort=10050|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# ListenIP=0.0.0.0|ListenIP=0.0.0.0|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# StartAgents=3|StartAgents=3|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|ServerActive=127.0.0.1|ServerActive=192.168.56.10:10051|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|Hostname=Zabbix server|Hostname=zabbix|' /etc/zabbix/zabbix_agentd.conf;
+sed -i 's|# HostnameItem=system.hostname|HostnameItem=system.hostname|' /etc/zabbix/zabbix_agentd.conf;
+
+
 systemctl enable zabbix-agent
 systemctl start zabbix-agent
 yum install zabbix-sender -y
 yum install zabbix-get -y
-
-#yum install zabbix-java-gateway -y
-#systemctl start zabbix-java-gateway
-#systemctl enable zabbix-java-gateway
 
 
 
